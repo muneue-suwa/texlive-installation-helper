db1abf39768da361639372d55215d6c6f8fed2e1
Muneue SUWA <dongsiku@s.okayama-u.ac.jp>
2021-11-09 23:52:23 +0900

    コメントアウトを追加した

--- run_tl_installer.ps1
+++ run_tl_installer.ps1
@@ -1,11 +1,17 @@
 #[void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+# 環境変数TEMPを一時的に書き換える．
 $Env:TEMP = "C:\temp_for_texlive"
 New-Item $Env:TEMP -ItemType Directory
+
 if ($? -eq $false) {
+    # C:\temp_for_texliveが既存の場合，
+    # エラーメッセージを表示し，削除を促す．
     Write-Host "Error: You should remove" $Env:TEMP
     exit
 }
 
+# ファイルを開くダイアログより，TeX Liveのinstall-tl-windows.batの絶対パスを取得する．
 Add-Type -AssemblyName System.Windows.Forms
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
     InitialDirectory = [Environment]::GetFolderPath('Desktop')
@@ -14,9 +20,11 @@ $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
 }
 
 if ($FileBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    # Cancelが押されなかった場合，install-tl-windows.batを実行する．
     Write-Host 'Filename:' $FileBrowser.FileName
     Start-Process $FileBrowser.FileName
 }
 else {
+    # それ以外の場合は，キャンセルされたことを表示する．
     Write-Host 'Cancelled'
 }
